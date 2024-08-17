2b1699a552508908504a7b60054a4e4aa82143cc
qwery1237 <94043757+qwery1237@users.noreply.github.com>
2024-08-16 17:49:40 -0700

    fix: #1 slider item resized

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,8 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { IMovie, ITVShow } from '../api';
 import { useOutletContext } from 'react-router-dom';
 import { IOutletContext } from '../App';
 import styled from 'styled-components';
+import useScreenSize from '../hooks/useScreenSize';
+import { getOffset } from '../utils';
+import Video from './Video';
 
 interface IProps {
   videos: IMovie[] | ITVShow[];
@@ -10,21 +13,122 @@ interface IProps {
   genre: string;
   isLastSlider: boolean;
 }
-const Wrapper = styled.div``;
-const Label = styled.h3`
-  font-size: 1.4vw;
-  line-height: 1.25vw;
+const Wrapper = styled.div`
+  width: 100%;
+  height: fit-content;
+  margin-bottom: 3vw;
+  position: relative;
+  z-index: 1;
+  top: -14vw;
 `;
+const Label = styled.h3`
+  margin-bottom: 2vw;
+  font-size: 1.8vw;
+  font-weight: 500;
+  padding: ${(props) => props.theme.paddingContainer};
+`;
+const SliderAnimationWrapper = styled.div<{ height: number }>`
+  width: 100%;
+  height: ${(props) => props.height + 'px'};
+  display: flex;
+  justify-content: center;
+`;
+const Row = styled.div<{ boxWidth: number }>`
+  display: flex;
+  gap: 4px;
+  position: absolute;
+`;
+
 export default function Slider({ videos, label, genre, isLastSlider }: IProps) {
+  const { width } = useScreenSize();
+  const [offset, setOffset] = useState(0);
+  const [startIndex, setStartIndex] = useState(0);
+  const [showPrev, setShowPrev] = useState(false);
+  const [showBtn, setShowBtn] = useState(false);
+  const [slideDirection, setSlideDirection] = useState('right');
+  const [leaving, setLeaving] = useState(false);
+  const [displaySlide, setDisplaySlide] = useState<null | (IMovie | ITVShow)[]>(
+    null
+  );
   const { handleOutletRendered } = useOutletContext<IOutletContext>();
+  const hasNext = videos.length > offset;
+  const remainingSpace = hasNext
+    ? 0
+    : offset - (displaySlide as IMovie[] | ITVShow[])?.length;
+  const toggleBtn = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.currentTarget.style.zIndex === '2'
+      ? (event.currentTarget.style.zIndex = '1')
+      : (event.currentTarget.style.zIndex = '2');
+
+    setShowBtn((prev) => !prev);
+  };
+  const boxWidth = (width * 0.92 - 4 * (offset + 1)) / offset;
+  const toggleLeaving = () => setLeaving((prev) => !prev);
+  useEffect(() => {
+    if (videos.length <= offset) {
+      setDisplaySlide(videos);
+      return;
+    }
+    if (startIndex + offset > videos.length) {
+      const startIndexToLast = videos.slice(startIndex);
+      const left = offset - startIndexToLast.length;
+      const firstToEndIndex = videos.slice(0, left);
+      const slide = [...startIndexToLast, ...firstToEndIndex];
+      setDisplaySlide(slide);
+      return;
+    }
+    const slide = videos.slice(startIndex, startIndex + offset);
+    setDisplaySlide(slide);
+  }, [videos, offset, startIndex]);
   useEffect(() => {
     if (!isLastSlider) return;
 
     handleOutletRendered(true);
   }, []);
+  useEffect(() => {
+    const newOffset = getOffset(width);
+    offset !== newOffset && setOffset(newOffset);
+  }, [width]);
+  useEffect(() => {
+    console.log(displaySlide, offset, hasNext);
+  }, [displaySlide]);
+  if (!videos.length || !displaySlide || !displaySlide.length) return <></>;
   return (
-    <Wrapper>
+    <Wrapper onMouseEnter={toggleBtn} onMouseLeave={toggleBtn}>
       <Label>{label}</Label>
+      <SliderAnimationWrapper height={(boxWidth * 6) / 11}>
+        <Row boxWidth={boxWidth}>
+          <Video
+            boxWidth={boxWidth}
+            video={videos[(startIndex - 1 + videos.length) % videos.length]}
+            contentExist={showPrev}
+          />
+          {displaySlide.map((video, i) => (
+            <Video
+              boxWidth={boxWidth}
+              video={video}
+              pointerEventExist
+              contentExist
+              index={i}
+              offset={offset}
+            />
+          ))}
+          {remainingSpace
+            ? [...Array(remainingSpace)].map((_, i) => (
+                <Video key={'remainingSpace_' + i} boxWidth={boxWidth} />
+              ))
+            : null}
+          <Video
+            boxWidth={boxWidth}
+            video={
+              hasNext
+                ? videos[(startIndex + offset) % videos.length]
+                : undefined
+            }
+            contentExist={hasNext}
+          />
+        </Row>
+      </SliderAnimationWrapper>
     </Wrapper>
   );
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,3 +1,174 @@
-export default function Video() {
-  return <div></div>;
+import styled from 'styled-components';
+import { IMovie, ITVShow } from '../api';
+import { makeImgPath } from '../utils';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
+import { motion } from 'framer-motion';
+
+interface Iprops {
+  boxWidth: number;
+  video?: IMovie | ITVShow;
+  pointerEventExist?: boolean;
+  contentExist?: boolean;
+  index?: number;
+  offset?: number;
+}
+interface IBoxProps {
+  boxWidth: number;
+  pointerEventExist?: boolean;
+  contentExist?: boolean;
+  index?: number;
+  offset?: number;
+}
+const Box = styled(motion.div)<IBoxProps>`
+  width: ${(props) => props.boxWidth + 'px'};
+  cursor: pointer;
+  ${(props) => (props.pointerEventExist ? '' : 'pointer-events: none;')}
+  ${(props) => (props.contentExist ? '' : 'opacity:0;')}
+  ${(props) => {
+    switch (props.index) {
+      case 0:
+        return 'transform-origin: center left;';
+      case props.offset && props.offset - 1:
+        return 'transform-origin: center right;';
+    }
+  }}
+  height: fit-content;
+  overflow: hidden;
+  border-radius: 3px;
+`;
+const VideoImg = styled(motion.div)<{ bgImg?: string }>`
+  width: 100%;
+  aspect-ratio: 11/6;
+  background-size: cover;
+  background-position: center;
+  background-image: url(${(props) => props.bgImg});
+`;
+const Info = styled(motion.div)`
+  width: 100%;
+  padding: 1rem;
+  opacity: 0;
+  display: none;
+
+  background-color: ${(props) => props.theme.black.darker};
+`;
+const BtnWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: end;
+  margin-bottom: 18%;
+`;
+const Btn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 3vw;
+  padding: 2px;
+  border-radius: 50%;
+  border: solid 1.5px rgba(255, 255, 255, 0.5);
+
+  &:hover {
+    cursor: pointer;
+    border-color: ${(props) => props.theme.white.darker};
+  }
+  @media (min-width: 800px) {
+    font-size: 2vw;
+  }
+`;
+const Title = styled.span`
+  @media (max-width: 500px) {
+    font-size: 3vw;
+  }
+`;
+const Rating = styled.div`
+  color: #46d369;
+  margin: 0.3vw 0;
+  @media (max-width: 500px) {
+    font-size: 3vw;
+  }
+`;
+
+const containerVariants = {
+  hover: {
+    borderRadius: '6px',
+    scale: 1.4,
+    y: -60,
+    boxShadow: 'rgba(0, 0, 0, 0.75) 0px 3px 10px',
+    minWidth: '150px',
+    transition: { delay: 0.3 },
+  },
+};
+const imgVariants = {
+  hover: {
+    borderRadius: '6px 6px 0 0',
+    transition: { delay: 0.3 },
+  },
+};
+const infoVariants = {
+  hover: {
+    display: 'block',
+    opacity: 1,
+    borderRadius: '0 0 6px 6px',
+    transition: { delay: 0.3 },
+  },
+};
+export default function Video({
+  video,
+  boxWidth,
+  pointerEventExist,
+  contentExist,
+  index,
+  offset,
+}: Iprops) {
+  const navigate = useNavigate();
+  const videoRef = useRef<HTMLDivElement>(null);
+  const { pathname } = useLocation();
+  const onClick = async () => {
+    navigate(String(`${pathname}?videoId=${video?.id}`), {
+      state: { genre: (video as IMovie).title ? 'movie' : 'tv' },
+    });
+  };
+
+  return (
+    <>
+      {contentExist ? (
+        <Box
+          ref={videoRef}
+          boxWidth={boxWidth}
+          pointerEventExist={pointerEventExist}
+          contentExist={contentExist}
+          variants={containerVariants}
+          whileHover='hover'
+          transition={{ type: 'tween', duration: 0.2 }}
+          index={index}
+          offset={offset}
+        >
+          <VideoImg
+            variants={imgVariants}
+            transition={{ type: 'tween', duration: 0.2 }}
+            bgImg={makeImgPath(video?.backdrop_path || '')}
+          />
+          <Info
+            variants={infoVariants}
+            transition={{ type: 'tween', duration: 0.2 }}
+          >
+            <BtnWrapper>
+              <Btn onClick={onClick}>
+                <MdKeyboardArrowDown />
+              </Btn>
+            </BtnWrapper>
+            <Rating>{video?.vote_average.toFixed(1)}/10</Rating>
+            <Title>{(video as IMovie).title || (video as ITVShow).name}</Title>
+          </Info>
+        </Box>
+      ) : (
+        <Box
+          boxWidth={boxWidth}
+          pointerEventExist={pointerEventExist}
+          contentExist={contentExist}
+        />
+      )}
+    </>
+  );
 }

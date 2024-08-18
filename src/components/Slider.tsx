cf234ae1d6f64da26845e4ce6fa8964203faed0c
qwery1237 <94043757+qwery1237@users.noreply.github.com>
2024-08-17 17:52:09 -0700

    fix: #2 Silder Button fixed

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -6,6 +6,10 @@ import styled from 'styled-components';
 import useScreenSize from '../hooks/useScreenSize';
 import { getOffset } from '../utils';
 import Video from './Video';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RiArrowLeftWideFill, RiArrowRightWideFill } from 'react-icons/ri';
+import { AnimatePresence } from 'framer-motion';
 
 interface IProps {
   videos: IMovie[] | ITVShow[];
@@ -36,9 +40,35 @@ const SliderAnimationWrapper = styled.div<{ height: number }>`
 const Row = styled.div<{ boxWidth: number }>`
   display: flex;
   gap: 4px;
-  position: absolute;
+  position: relative;
 `;
 
+const PrevBtn = styled.div<{ height: number; showBtn: boolean }>`
+  width: 4vw;
+  height: ${(props) => props.height + 'px'};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${(props) => (props.showBtn ? '' : 'color: transparent;')};
+  font-size: 3vw;
+  position: absolute;
+  left: 0;
+  border-top-right-radius: 3px;
+  border-bottom-right-radius: 3px;
+  background-color: rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+  transition: font-size 0.2s ease-in-out;
+  &:hover {
+    font-size: 6vw;
+    background-color: rgba(0, 0, 0, 0.7);
+  }
+`;
+const NextBtn = styled(PrevBtn)`
+  border-radius: 0;
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+  left: 96vw;
+`;
 export default function Slider({ videos, label, genre, isLastSlider }: IProps) {
   const { width } = useScreenSize();
   const [offset, setOffset] = useState(0);
@@ -52,6 +82,7 @@ export default function Slider({ videos, label, genre, isLastSlider }: IProps) {
   );
   const { handleOutletRendered } = useOutletContext<IOutletContext>();
   const hasNext = videos.length > offset;
+  const boxWidth = (width * 0.92 - 4 * (offset + 1)) / offset;
   const remainingSpace = hasNext
     ? 0
     : offset - (displaySlide as IMovie[] | ITVShow[])?.length;
@@ -59,13 +90,24 @@ export default function Slider({ videos, label, genre, isLastSlider }: IProps) {
     event.currentTarget.style.zIndex === '2'
       ? (event.currentTarget.style.zIndex = '1')
       : (event.currentTarget.style.zIndex = '2');
-
-    setShowBtn((prev) => !prev);
   };
-  const boxWidth = (width * 0.92 - 4 * (offset + 1)) / offset;
+  const toggleShowbtn = () => setShowBtn((prev) => !prev);
   const toggleLeaving = () => setLeaving((prev) => !prev);
+  const increaseIndex = () => {
+    !showPrev && setShowPrev(true);
+    // if (leaving) return;
+    // toggleLeaving();
+    setStartIndex((prev) => (prev + offset) % videos.length);
+  };
+  const decreaseIndex = () => {
+    // if (leaving) return;
+    // toggleLeaving();
+    setStartIndex((prev) => (prev - offset + videos.length) % videos.length);
+  };
   useEffect(() => {
     if (videos.length <= offset) {
+      setStartIndex(0);
+      setShowPrev(false);
       setDisplaySlide(videos);
       return;
     }
@@ -96,38 +138,68 @@ export default function Slider({ videos, label, genre, isLastSlider }: IProps) {
   return (
     <Wrapper onMouseEnter={toggleBtn} onMouseLeave={toggleBtn}>
       <Label>{label}</Label>
-      <SliderAnimationWrapper height={(boxWidth * 6) / 11}>
-        <Row boxWidth={boxWidth}>
-          <Video
-            boxWidth={boxWidth}
-            video={videos[(startIndex - 1 + videos.length) % videos.length]}
-            contentExist={showPrev}
-          />
-          {displaySlide.map((video, i) => (
+      <SliderAnimationWrapper
+        height={(boxWidth * 6) / 11}
+        onMouseEnter={toggleShowbtn}
+        onMouseLeave={toggleShowbtn}
+      >
+        <AnimatePresence onExitComplete={toggleLeaving}>
+          <Row boxWidth={boxWidth}>
             <Video
               boxWidth={boxWidth}
-              video={video}
-              pointerEventExist
-              contentExist
-              index={i}
-              offset={offset}
+              video={
+                hasNext
+                  ? videos[(startIndex - 1 + videos.length) % videos.length]
+                  : undefined
+              }
+              contentExist={showPrev && hasNext}
             />
-          ))}
-          {remainingSpace
-            ? [...Array(remainingSpace)].map((_, i) => (
-                <Video key={'remainingSpace_' + i} boxWidth={boxWidth} />
-              ))
-            : null}
-          <Video
-            boxWidth={boxWidth}
-            video={
-              hasNext
-                ? videos[(startIndex + offset) % videos.length]
-                : undefined
-            }
-            contentExist={hasNext}
-          />
-        </Row>
+            {displaySlide.map((video, i) => (
+              <Video
+                boxWidth={boxWidth}
+                video={video}
+                pointerEventExist
+                contentExist
+                index={i}
+                offset={offset}
+              />
+            ))}
+            {remainingSpace
+              ? [...Array(remainingSpace)].map((_, i) => (
+                  <Video key={'remainingSpace_' + i} boxWidth={boxWidth} />
+                ))
+              : null}
+            <Video
+              boxWidth={boxWidth}
+              video={
+                hasNext
+                  ? videos[(startIndex + offset) % videos.length]
+                  : undefined
+              }
+              contentExist={hasNext}
+            />
+          </Row>
+        </AnimatePresence>
+        {hasNext ? (
+          <>
+            {showPrev ? (
+              <PrevBtn
+                showBtn={showBtn}
+                onClick={decreaseIndex}
+                height={(boxWidth * 6) / 11}
+              >
+                <RiArrowLeftWideFill />
+              </PrevBtn>
+            ) : null}
+            <NextBtn
+              showBtn={showBtn}
+              onClick={increaseIndex}
+              height={(boxWidth * 6) / 11}
+            >
+              <RiArrowRightWideFill />
+            </NextBtn>
+          </>
+        ) : null}
       </SliderAnimationWrapper>
     </Wrapper>
   );
